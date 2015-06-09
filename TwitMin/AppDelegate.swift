c1eafe89fd871a43b4dc2d8808575d56ad6a6b59
Peter Wunder <peterwunder@peterwunder.de>
2015-06-09 21:20:10 +0200

    Initial Commit

---
+++ TwitMin/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TwitMin
+//
+//  Created by Peter Wunder on 09.06.15.
+//  Copyright © 2015 Peter Wunder. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
